08a25f677486ceb3db24ffcf801f146ae3e9ea08
Flexksx <cristian.cretu2808@gmail.com>
2024-10-07 17:06:59 +0300

    remove all old

--- SOLID/app/src/main/java/payment/processor/PaymentProcessor.java
+++
@@ -1,7 +0,0 @@
-package main.java.payment.processor;
-
-import main.java.payment.PaymentMethod;
-
-public interface PaymentProcessor {
-    public void processPayment(PaymentMethod paymentMethod, double amount);
-}
